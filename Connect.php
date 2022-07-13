a1348e17a58736940bb320dc13ee7588160e45ce
ArunPrabuMurugesan <arunprabu.murugesan@gmail.com>
2022-07-13 14:46:46 +0530

    committed Connect.php in branch2

--- Connect.php
+++ Connect.php
@@ -2,8 +2,8 @@
   $host = "127.0.0.1";
   $usr = "root";
   $pass = "";
-  $conn = mysql_connect($host,$usr,$pass) or die("Host Connection Fails");
+  $conn = mysqli_connect($host,$usr,$pass) or die("Host Connection Fails");
   $db = "properties";
-  mysql_select_db($db,$conn) or die("Could Not Connect MySQL DataBase");
+  mysqli_select_db($db,$conn) or die("Could Not Connect MySQL DataBase");
   error_reporting(0);
 ?>
